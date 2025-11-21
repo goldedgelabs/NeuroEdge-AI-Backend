46256c5c724414c44d093ec9d1287f66ee170b52
goldedgelabs <goldedgelabs@gmail.com>
2025-11-21 10:45:54 +0300

    Create index.ts

---
+++ NeuroEdge-Shared/governance/index.ts
@@ -0,0 +1,5 @@
+export * from "./middleware/security.middleware";
+export * from "./founders/founder.service";
+export * from "./doctrine/protection";
+export * from "./risk/folder-intel";
+export * from "./detectors/misuse";
